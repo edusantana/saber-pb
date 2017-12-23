77597e1af50ad0021b3fd8c94c8ad92e90985543
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2017-12-23 09:56:23 -0300

    versão inicial

---
+++ saber.js
@@ -0,0 +1,106 @@
+
+// Adicionar tecla de atalho ao link "Novo registro de aula" ALT+N
+// Procura por link ".*/new"
+// Exemplo:
+// <a class="btn btn-new btn-success pull-right" href="/platform/teachings/320557/class_logs/new"><i class="icon-file-alt"></i>
+// Novo registro de aula
+// </a>
+// http://www.saber.pb.gov.br/platform/teachings/320557/class_logs
+// Adicionar tecla de atalho ALT+n para criar novos elementos
+var links = document.getElementsByTagName('a');
+for(i=0; i<links.length; i++){
+    if (links[i].href.endsWith("/new")){
+        links[i].accessKey='n';
+        links[i].title='Tecla de atalho adicionada: Alt+n';
+    }
+}
+
+// Adicionar tecla de atalho ALT+s para salvar
+var botoes = document.getElementsByTagName('button');
+for(i=0; i<botoes.length; i++){
+    b = botoes[i]
+    if (b.textContent.endsWith("Salvar")){
+        b.accessKey='s';
+        b.title='Tecla de atalho adicionada: Alt+s';
+    }
+    if (b.textContent.includes("Adicionar")){
+        b.accessKey='n';
+        b.title='Tecla de atalho adicionada: Alt+n';
+    }
+}
+
+
+var breadcrumbs = document.querySelector("body > div.breadcrumbs > div");
+var colagem = document.createElement("textarea");
+colagem.id = "colagem";
+colagem.cols = "100";
+colagem.rows = "4";
+colagem.placeholder = "Cole aqui as colunas copiadas do Excel, depois pressione TAB e ALT+s para salvar"
+
+
+/* =========== REGISTRO DE FREQUÊNCIA  ==================*/
+function atualizaRegistroDeFrequencia(){
+    console.log('Valor mudou!')
+    // dia: class_frequency[day]
+
+    var valor_colado = document.getElementById('colagem').value;
+    var valores = valor_colado.split("\t");
+
+    console.log('Valores:' + valores);
+    
+    // document.getElementById('class_frequency_day').value = '21/12/2017'
+    if (valores.length > 0){
+        document.getElementById('class_frequency_day').value = valores[0];    
+    }
+    if (valores.length > 1){
+        document.getElementById('class_frequency_started_at').value = valores[1];
+    }
+    if (valores.length > 2){
+        document.getElementById('class_frequency_finished_at').value = valores[2];
+    }
+    if (valores.length > 3){
+        document.getElementById('class_frequency_justification').value = valores[2];
+    }    
+    return;
+}
+
+if (window.location.pathname.includes("class_frequencies/new")){
+    console.log('Adicionando input de registro de frequência')    
+    colagem.addEventListener("change", atualizaRegistroDeFrequencia);
+    breadcrumbs.appendChild(colagem); //appendChild
+    colagem.focus();
+}
+/* =======================================================*/
+
+/* ============= REGISTRO DE AULA ========================*/
+function atualizaRegistroDeAula(){
+    console.log('Valor mudou!')
+    // dia: class_frequency[day]
+
+    var valor_colado = document.getElementById('colagem').value;
+    var valores = valor_colado.split("\t");
+
+    console.log('Valores:' + valores);
+    
+    if (valores.length > 0){
+        document.getElementById('class_log_day').value = valores[0];    
+    }
+    if (valores.length > 1){
+        document.getElementById('class_log_classes').value = valores[1];
+    }
+    if (valores.length > 2){
+        document.getElementById('class_log_content').value = valores[2];
+    }
+    if (valores.length > 3){
+        document.getElementById('class_log_activity').value = valores[2];
+    }    
+    return;
+}
+
+if (window.location.pathname.includes("class_logs/new")){
+    console.log('Adicionando input de registro de aula')    
+    colagem.addEventListener("change", atualizaRegistroDeAula);
+    breadcrumbs.appendChild(colagem); //appendChild
+    colagem.focus();
+}
+/* =======================================================*/
